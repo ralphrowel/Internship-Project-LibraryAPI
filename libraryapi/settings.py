16bd46f960d657a718f8af48085e18a7c45e0566
ralphrowel <ralphrowel00@gmail.com>
2025-10-13 11:42:14 +0800

    added postgreSQL

--- libraryapi/settings.py
+++ libraryapi/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'libraryapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'libraryapi_db',
+        'USER': 'postgres',
+        'PASSWORD': 'March282004.',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
